29bf94c83de196378d4cc76264c33430d65c05de
jun <jun.lee3@baruchmail.cuny.edu>
2024-05-14 23:20:38 -0400

    sql and extract code push

--- scripts/BCI_dw.sql
+++ scripts/BCI_dw.sql
@@ -1,60 +1,63 @@
-CREATE SCHEMA INSTANCE;
+CREATE SCHEMA IF NOT EXISTS "bic";
 
-CREATE TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_account ( 
-	account_id int64 NOT NULL  ,
-	BIC_number int64  ,
-	account_name string  ,
-	account_type string  
+CREATE  TABLE "bic".dim_account (
+	account_id           INT  NOT NULL  ,
+	bic_number           INT    ,
+	account_name         VARCHAR(100)    ,
+	account_type         VARCHAR(255)    ,
+	CONSTRAINT pk_dim_account PRIMARY KEY ( account_id )
  );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_account ADD PRIMARY KEY ( account_id )  NOT ENFORCED;
-
-CREATE TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_date ( 
-	date_id int64 NOT NULL  ,
-	Date_original date  ,
-	date_Iso datetime  ,
-	year int64  ,
-	quarter int64  ,
-	month int64  
+CREATE  TABLE "bic".dim_date (
+	date_id              INT  NOT NULL  ,
+	date_original        DATE    ,
+	date_iso             DATE    ,
+	"year"               INT    ,
+	quarter              INT    ,
+	"month"              INT    ,
+	CONSTRAINT pk_dim_date PRIMARY KEY ( date_id )
  );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_date ADD PRIMARY KEY ( date_id )  NOT ENFORCED;
-
-CREATE TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_location ( 
-	location_id int64 NOT NULL  ,
-	borough string  ,
-	Latitude bignumeric  ,
-	Longtitude bignumeric  
+CREATE  TABLE "bic".dim_location (
+	location_id          INT  NOT NULL  ,
+	borough              VARCHAR(255)    ,
+	latitude             Decimal    ,
+	longtitude           Decimal    ,
+	CONSTRAINT pk_dim_location PRIMARY KEY ( location_id )
  );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_location ADD PRIMARY KEY ( location_id )  NOT ENFORCED;
-
-CREATE TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_violation ( 
-	violation_id int64 NOT NULL  ,
-	type_violation string  ,
-	violation_number string  ,
-	violation_phase string  
+CREATE  TABLE "bic".dim_violation (
+	violation_id         INT  NOT NULL  ,
+	type_violation       VARCHAR(255)    ,
+	violation_number     VARCHAR(255)    ,
+	violation_phase      VARCHAR(255)    ,
+	CONSTRAINT pk_dim_violation PRIMARY KEY ( violation_id )
  );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_violation ADD PRIMARY KEY ( violation_id )  NOT ENFORCED;
-
-CREATE TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.facts_fine ( 
-	fact_id int64 NOT NULL  ,
-	fine_amount numeric(10,2)  ,
-	maximum_fine numeric(10,2)  ,
-	violation_number string  ,
-	location_id int64 NOT NULL  ,
-	date_id int64  ,
-	violation_id int64  ,
-	account_id int64  
+CREATE  TABLE "bic".facts_fine (
+	fact_id              BIGINT  NOT NULL  ,
+	fine_amount          DECIMAL(10,2)    ,
+	maximum_fine         DECIMAL(10,2)    ,
+	violation_number     VARCHAR(255)    ,
+	location_id          INT  NOT NULL  ,
+	date_id              INT    ,
+	violation_id         INT    ,
+	account_id           INT    ,
+	CONSTRAINT pk_facts_fine PRIMARY KEY ( fact_id, location_id )
  );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.facts_fine ADD PRIMARY KEY ( fact_id, location_id )  NOT ENFORCED;
+CREATE UNIQUE INDEX unq_account_id ON "bic".facts_fine ( account_id );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.facts_fine ADD CONSTRAINT fk_facts_fine_dim_account FOREIGN KEY ( account_id ) REFERENCES `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_account( account_id ) NOT ENFORCED;
+CREATE UNIQUE INDEX unq_violation_id ON "bic".facts_fine ( violation_id );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.facts_fine ADD CONSTRAINT fk_facts_fine_dim_violation FOREIGN KEY ( violation_id ) REFERENCES `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_violation( violation_id ) NOT ENFORCED;
+CREATE UNIQUE INDEX unq_date_id ON "bic".facts_fine ( date_id );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.facts_fine ADD CONSTRAINT fk_facts_fine_dim_date FOREIGN KEY ( date_id ) REFERENCES `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_date( date_id ) NOT ENFORCED;
+CREATE UNIQUE INDEX unq_location_id ON "bic".facts_fine ( location_id );
 
-ALTER TABLE `BIC Violation_dw_lgl(logical)`.INSTANCE.facts_fine ADD CONSTRAINT fk_facts_fine_dim_location FOREIGN KEY ( location_id ) REFERENCES `BIC Violation_dw_lgl(logical)`.INSTANCE.dim_location( location_id ) NOT ENFORCED;
+ALTER TABLE "bic".facts_fine ADD CONSTRAINT fk_facts_fine_dim_account FOREIGN KEY ( account_id ) REFERENCES "bic".dim_account( account_id );
+
+ALTER TABLE "bic".facts_fine ADD CONSTRAINT fk_facts_fine_dim_violation FOREIGN KEY ( violation_id ) REFERENCES "bic".dim_violation( violation_id );
+
+ALTER TABLE "bic".facts_fine ADD CONSTRAINT fk_facts_fine_dim_date FOREIGN KEY ( date_id ) REFERENCES "bic".dim_date( date_id );
+
+ALTER TABLE "bic".facts_fine ADD CONSTRAINT fk_facts_fine_dim_location FOREIGN KEY ( location_id ) REFERENCES "bic".dim_location( location_id );
